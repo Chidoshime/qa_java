e43a0a4fefe2c5f011426c2bc951910b49b315db
Sergey Storozhilov <chidoshimeya@gmail.com>
2023-03-31 20:02:41 +0300

    new try restore my repo

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,44 @@
+import com.example.Feline;
+import org.junit.Test;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTest {
+
+
+    @Test
+    public void felineEatMeatReturnPredator() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
+
+        assertEquals("Вид пищи не совпадает", expectedFood, feline.getFood("Хищник"));
+    }
+
+    @Test
+    public void felineGetFamilyReturnCat() {
+        Feline feline = new Feline();
+        String expectedFamily = "Кошачьи";
+
+        assertEquals("Семейство не совпадает", expectedFamily, feline.getFamily());
+    }
+
+    @Test
+    public void felineGetKittensEmptyReturnOne() {
+        Feline feline = new Feline();
+        int expectedKittenCount = 1;
+
+        assertEquals("Количество котят не совпадает", expectedKittenCount, feline.getKittens());
+    }
+
+    @Test
+    public void felineGetKittensSpecifiedReturnSpecified() {
+        Feline feline = new Feline();
+        int expectedKittenCount = 5;
+
+        assertEquals("Количество котят не совпадает", expectedKittenCount, feline.getKittens(5));
+    }
+}
